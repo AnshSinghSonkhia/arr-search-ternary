249e7014c21cd683f2787c8a6b4bdf1c48701f3d
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2025-03-23 11:09:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const ternarySearchMin = require("./src/ternarySearchMin");
+const ternarySearchMax = require("./src/ternarySearchMax");
+const ternarySearchArray = require("./src/ternarySearchArray");
+const ternarySearchRotatedArray = require("./src/ternarySearchRotatedArray");
+const ternarySearchDiscrete = require("./src/ternarySearchDiscrete");
+
+module.exports = {
+    ternarySearchMin,
+    ternarySearchMax,
+    ternarySearchArray,
+    ternarySearchRotatedArray,
+    ternarySearchDiscrete
+};
